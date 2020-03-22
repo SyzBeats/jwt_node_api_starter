283836b36dca16c266f05c6dd8fa8a4223a1f8d6
Simeon Zimmermann <syzdevacc@gmail.com>
2020-03-22 13:36:11 +0100

    changed return in middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -20,6 +20,6 @@ module.exports = function(req, res, next) {
     req.user = decoded.user;
     next();
   } catch (error) {
-    res.status(401).json({ message: "token not valid" });
+    return res.status(401).json({ message: "token not valid" });
   }
 };
